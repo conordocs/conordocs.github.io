f9b819403249c032da643aa84af6b9b9cc2c7e20
Conor Doherty <3056662@ads.qub.ac.uk>
2022-05-24 16:51:09 +0100

    contact form fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
     <div
       name="contact"
       id="contact"
-      className="w-full h-full bg-[#0a192f] flex justify-center items-center p-4 pb-5"
+      className="w-full h-full bg-[#0a192f] flex justify-center items-center p-4 text-gray-300 pb-5"
     >
       <form
         method="POST"
@@ -15,13 +15,17 @@ const Contact = () => {
         className="flex flex-col max-w-[600px] w-full"
       >
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-[#11d670] text-gray-300">
+          <p className="text-4xl font-bold inline border-b-4 border-[#11d670]">
             Contact
           </p>
           {/* eslint-disable-next-line */}
-          <p className="text-gray-300 py-4">
-            // Submit the form below or send me an email @{" "}
-            <a href="mailto:conordoherty97@hotmail.com">
+          <p className="pt-4 text-2xl">
+            Submit the form below or send me an email{" "}
+            <span className="text-[#11d670]">@ </span>
+            <a
+              className="hover:text-[#11d670]"
+              href="mailto:conordoherty97@hotmail.com"
+            >
               conordoherty97@hotmail.com
             </a>
           </p>
