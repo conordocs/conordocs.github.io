514df464a3fd1712f538f50bbae2753f885b7cd1
Conor Doherty <3056662@ads.qub.ac.uk>
2022-05-24 15:05:21 +0100

    updates

---
+++ src/components/ProjectItem.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ProjectItem = ({ title, backgroundImg, projectUrl, projectType }) => {
+  return (
+    <div className="relative flex items-center justify-center h-auto w-full shadow-gray-400 rounded-xl p-2 group hover:bg-gradient-to-r from-[#11d670] to-[#709dff] text-white font-bold">
+      <img
+        className="rounded-xl group-hover:opacity-10"
+        src={backgroundImg}
+        alt="/"
+      />
+      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <h3 className="text-2xl text-white tracking-wider text-center">
+          {title}
+        </h3>
+        <p className="pb-4 pt-2 text-white text-center">{projectType}</p>
+        <a href={projectUrl}>
+          <p className="p-5 text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
+            More Info
+          </p>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectItem;
